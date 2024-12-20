87c1e3e2afee442e3b0b8f2efe4d7e61b3243f90
Tim <tim103@pm.me>
2024-12-20 14:54:16 +1100

    Added server caching with Nitro.

--- layers/palette/server/api/palettes/colors/index.get.ts
+++ layers/palette/server/api/palettes/colors/index.get.ts
@@ -1,9 +1,9 @@
 import type { GetColorsDto } from '../../../dtos/palette.dto';
 
-export default defineEventHandler(async (): Promise<GetColorsDto> => {
+export default defineCachedEventHandler(async (): Promise<GetColorsDto> => {
   const response = await modules.palette.service.getColorNames();
 
   return {
     items: response
   };
-});
+}, { maxAge: 60 * 60 });
